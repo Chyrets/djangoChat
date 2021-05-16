a74ce2c580d91ea3aa61aa211cfacd04a55fa5e4
chyrets <uladzislawchyrets@gmail.com>
2021-05-16 20:13:09 +0300

    add authy to project

---
+++ authy/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserSignupForm(forms.ModelForm):
+    """
+    User signup form
+    """
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+
+        if commit:
+            user.save()
+        return user
--- authy/views.py
+++ authy/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
 
-# Create your views here.
+from authy.forms import UserSignupForm
+
+
+class UserSignupView(CreateView):
+    """
+    User signup view
+    """
+    model = User
+    form_class = UserSignupForm
+    success_url = '/'
+    template_name = 'authy/signup.html'
+
+    def form_valid(self, form):
+        form_valid = super().form_valid(form)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        auth_user = authenticate(username=username, password=password)
+        login(self.request, auth_user)
+        return form_valid
